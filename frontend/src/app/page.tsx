195d85dcdb206a931b920ebc4988203b18783676
Manmit Tiwade <112580293+Manmit124@users.noreply.github.com>
2024-07-28 19:25:22 +0530

    adding frontend and backend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=''>
+      <h1>Hello Brother</h1>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
